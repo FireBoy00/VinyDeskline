1bc3228d68918ee62385637908074477f1f58145
FireBoy <thecutefireboy@gmail.com>
2025-11-17 13:51:55 +0100

    Re-Add all asset files to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // General
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/bootstrap.js',
+
+                // Dashboard
+                'resources/css/dashboard.css',
+                'resources/js/dashboard.js',
+                
+                // Home
+                'resources/css/home.css',
             ],
             refresh: true,
         }),
