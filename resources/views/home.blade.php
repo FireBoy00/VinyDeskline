6e357dd6dbd80b92c4f2090a5c2d2947722545ed
gabija <gasta24@student.sdu.dk>
2025-12-14 11:58:56 +0100

    Added one library for the mqtt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VinyDeskline</title>
+    
+    <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @vite(['resources/css/general-user.css'])
